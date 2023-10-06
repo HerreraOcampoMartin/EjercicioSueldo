8dff74362bd66b6b785e4329732391e1064edcff
Martín <herreraocampomartin@gmail.com>
2023-10-06 20:28:33 -0300

    Calculo terminado (?)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,39 @@
 
 using namespace std;
 
+class BonoPresentismo {
+public:
+    float calcularBonoPresentismo(int diasAusente) {
+        float bono = 100.0;
+
+        if(diasAusente == 1){
+            bono = 50;
+        } else if(diasAusente > 1) {
+            bono = 0;
+        }
+
+        return bono;
+    }
+};
+
+class BonoResultados {
+public:
+    float calcularBonoResultado(int tipoBono, float sueldoNeto) {
+        // 1: 10% del sueldo neto
+        // 2: $80 fijos
+        // 3: nada
+        float bono = 0;
+
+        if(tipoBono == 1) {
+            bono = 0.1f * sueldoNeto;
+        } else if(tipoBono == 2) {
+            bono = 80.0;
+        }
+
+        return bono;
+    }
+};
+
 class Categoria {
 private:
     string denominacionCategoria;
@@ -25,10 +58,24 @@ class Persona {
 private:
     string nombre;
     Categoria *categoriaTrabajador;
+    float sueldo;
 public:
     Persona(string nombre, Categoria *categoriaTrabajador) {
         this->nombre = nombre;
         this->categoriaTrabajador = categoriaTrabajador;
+        this->sueldo = categoriaTrabajador->getSueldoBase();
+    }
+
+    float calcularSueldo(int diasAusente, int tipoBonoResultados) {
+        BonoPresentismo bp;
+        BonoResultados br;
+
+        float extraPresentismo = bp.calcularBonoPresentismo(diasAusente);
+        float extraResultados = br.calcularBonoResultado(tipoBonoResultados, categoriaTrabajador->getSueldoBase());
+
+        sueldo = categoriaTrabajador->getSueldoBase() + extraPresentismo + extraResultados;
+
+        return sueldo;
     }
 
     string getNombre() {
@@ -38,9 +85,38 @@ public:
     Categoria* getCategoriaTrabajador() {
         return this->categoriaTrabajador;
     }
+
+    float getSueldo() {
+        return this->sueldo;
+    }
+
+    void imprimirDatos() {
+        cout << nombre << " - " << categoriaTrabajador->getDenominacionCategoria() << " - $" << sueldo << endl;
+    }
+};
+
+class Inicio {
+public:
+    Inicio() {
+        Categoria *gerente = new Categoria("Gerente", 1000);
+        Categoria *cadete = new Categoria("Cadete", 1500);
+
+        Persona pepe("Pepe", cadete);
+
+        pepe.imprimirDatos();
+
+        // 1: 10% del sueldo neto
+        // 2: $80 fijos
+        // 3: nada
+        pepe.calcularSueldo(2, 1);
+        pepe.imprimirDatos();
+    }
 };
 
 int main() {
+
+    Inicio i;
+
 
 
 
