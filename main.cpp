16e4f7de911cc55d8f83d0c7890d507c0dd57610
Martín <herreraocampomartin@gmail.com>
2023-10-06 19:55:30 -0300

    Agregar clases Categoria y Persona

--- main.cpp
+++ main.cpp
@@ -2,7 +2,47 @@
 
 using namespace std;
 
+class Categoria {
+private:
+    string denominacionCategoria;
+    float sueldoBase;
+public:
+    Categoria(string denominacionCategoria, float sueldoBase) {
+        this->denominacionCategoria = denominacionCategoria;
+        this->sueldoBase = sueldoBase;
+    }
+
+    string getDenominacionCategoria() {
+        return this->denominacionCategoria;
+    }
+
+    float getSueldoBase() {
+        return this->sueldoBase;
+    }
+};
+
+class Persona {
+private:
+    string nombre;
+    Categoria *categoriaTrabajador;
+public:
+    Persona(string nombre, Categoria *categoriaTrabajador) {
+        this->nombre = nombre;
+        this->categoriaTrabajador = categoriaTrabajador;
+    }
+
+    string getNombre() {
+        return this->nombre;
+    }
+
+    Categoria* getCategoriaTrabajador() {
+        return this->categoriaTrabajador;
+    }
+};
+
 int main() {
-    cout << "Hello, World!" << endl;
+
+
+
     return 0;
 }
